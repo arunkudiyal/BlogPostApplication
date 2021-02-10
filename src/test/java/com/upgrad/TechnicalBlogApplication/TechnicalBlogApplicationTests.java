dabad46170e27447c463e9fafb668bb250c0d042
Arun Kudiyal <arunkudiyal@icloud.com>
2021-02-10 14:45:02 +0530

    Code Updated

---
+++ src/test/java/com/upgrad/TechnicalBlogApplication/TechnicalBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upgrad.TechnicalBlogApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechnicalBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
